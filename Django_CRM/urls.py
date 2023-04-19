b77d1d06ce17f5a98e8a976886fd157c52ace282
aurkhan999 <aurkhan999@gmail.com>
2023-04-19 14:41:28 +0500

    registration

--- Django_CRM/urls.py
+++ Django_CRM/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
     # path('admin/doc/', include('django.contrib.admindocs.urls'))
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been logged in welcome home page.")
+            return redirect('home')
+        else:
+            messages.success(request, "There was ans error please try again.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
